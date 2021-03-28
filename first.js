64f1d54d1b243d0a2d5eb4cff83ed6a016c2cbe1
KindrakGena <kindrak1985@gmail.com>
2021-03-28 18:35:57 +0300

    html+js

---
+++ first.js
@@ -0,0 +1,338 @@
+//Завдання №1
+
+/*let q = 'hello';
+let w = 'owu';
+let e = 'com';
+let r = 'ua';
+let numberOne = 1;
+let numberThen = 10;
+let minusNumber = -999;
+let numberHundred = 123;
+let numberPi = 3.14;
+let numberTwo = 2.7;
+let numberSixteen = 16;
+let numberBul = true;
+let numberBulFalse = false;
+
+
+console.log(q,w, e, r, numberOne, numberTwo, numberBulFalse, numberSixteen, numberBul, numberPi, numberHundred, numberThen);
+
+
+document.write(r, q, w, r, e, numberThen, numberOne, numberTwo, numberBulFalse, numberSixteen, numberBul, numberPi, numberHundred);
+
+
+alert(q);
+alert (w);
+alert (e);
+alert (r);
+alert(numberOne);
+alert(numberThen);
+alert(minusNumber);
+alert(numberHundred);
+alert(numberPi);
+alert(numberTwo);
+alert(numberSixteen);
+alert(numberBul);
+alert(numberBulFalse);
+
+
+// Завдання №2
+
+ q = 'bye';
+ w = 'okten';
+ e = 'dot';
+ r = `uk`;
+ numberOne = 11;
+ numberThen = 1;
+ minusNumber = 999;
+ numberHundred = 321;
+ numberPi = 14.3;
+ numberTwo = 5.7;
+ numberSixteen = 18;
+ numberBul = false;
+ numberBulFalse = true;
+
+console.log(q,w, e, r, numberOne, numberTwo, numberBulFalse, numberSixteen, numberBul, numberPi, numberHundred, numberThen);
+
+
+document.write(r, q, w, r, e, numberThen, numberOne, numberTwo, numberBulFalse, numberSixteen, numberBul, numberPi, numberHundred);
+
+alert(q);
+alert (w);
+alert (e);
+alert (r);
+alert(numberOne);
+alert(numberThen);
+alert(minusNumber);
+alert(numberHundred);
+alert(numberPi);
+alert(numberTwo);
+alert(numberSixteen);
+alert(numberBul);
+alert(numberBulFalse);*/
+
+//Завдання №3
+
+/*const myName = prompt('Вітаю Євген');
+const myFatherName = prompt('Вітаю Євген Миронович');
+const myAge = prompt('Тобі 35 років');
+
+// console.log(myAge);
+// console.log(myName);
+// console.log(myFatherName);
+
+console.log(`${myName} ${myFatherName} ${myAge}`);*/
+
+
+//Завдання №4
+
+/*let aboutMy = myName + ' ' + myFatherName + ' ' + myAge ;
+
+console.log(aboutMy);*/
+
+//Завдання №5
+
+/*let num1 = + prompt('one');
+let num2 = + prompt('two');
+let num3 = + prompt('three');
+
+console.log(num1);
+console.log(num2);
+console.log(num3);*/
+
+// Завдання №6
+
+
+// Завдання №11
+
+/*let a= 100;
+let b = '100';
+let c= true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+*/
+
+
+//Завдання №9
+// let firstNam  = Math.round( prompt('numb'));
+// let secondNum = Math.round(prompt('second'));
+// let thirdNum = Math.round(prompt('third'));
+//
+// console.log(firstNam);
+// console.log(secondNum);
+// console.log(thirdNum);
+// let total = firstNam + secondNum + thirdNum;
+// console.log(total);
+
+//Піднесення до степення
+
+/*let numBase = prompt('Основа');
+let  numExp =prompt('Експонента');
+
+console.log(Math.pow(numBase, numExp));*/
+
+//ParseIn
+
+// let numPrIn1 = parseInt(prompt('перше'));
+// let numPrIn2 = parseInt(prompt('друге'));
+// let numPrIn3 = parseInt(prompt('третє'));
+// let numPrIn4 = parseInt(prompt('четверте'))
+//
+// let sumPrIn = numPrIn1 + numPrIn2 + numPrIn3 + numPrIn4;
+// console.log(sumPrIn);
+
+
+//ParseFloat
+/*
+let numPrFl1 = parseFloat(prompt('перше'));
+let numPrFl2 = parseFloat(prompt('друге'));
+let numPrFl3 = parseFloat(prompt('третє'));
+
+let result = numPrFl1 + numPrFl2 + numPrFl3 ;
+console.log(result);
+
+ */
+
+// logical operators 
+
+//console.log(`${5<6} ${5>6} ${5 == 6} ${5===6} ${10>=10} ${10===10} ${10 !== 10} ${10<10} ${10>10} ${123 ==='123'} ${123 =='123'}`);
+
+
+//           РОБОТА в Аудиторії
+
+// Завданя №1
+/*let str = 'Привіт';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log( typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+*/
+
+//Завдання №3
+/*let a6 = 5 % 3;
+console.log(a6);
+let a7 = 3 % 5;
+console.log(a7);
+let a8 = 5 + '3';
+console.log(a8);
+let a9 = '5' - 3;
+console.log(a9);
+let a10 = 75 + 'кг';
+console.log(a10);*/
+
+//Завдання №4
+
+/*let height = 23;
+let width = 10;
+let s = height + width;
+console.log(s);
+
+ */
+//завдання №5
+
+/*let heightC = 10;
+let dC = 4;
+let rC = dC / 2;
+let pi = 3.14
+console.log(pi);
+let v = pi * Math.pow(rC , 2) * heightC
+console.log(v);*/
+
+// Завдання №6
+
+/*let n = 3;
+let m = 4;
+let mPow = Math.pow(m ,2);
+console.log(mPow);
+let nPow = Math.pow(n , 2);
+console.log(nPow);
+let mN = nPow + mPow;
+console.log(mN);
+let k = Math.sqrt(mN)
+console.log(k);*/
+
+/*let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(m ,2) + Math.pow(n , 2))
+console.log(k);*/
+
+// Завдання №7
+
+/*let str = '"Hello world"';
+console.log(str);
+document.write(str);
+alert(str);*/
+
+//Завдання № 8
+/*let userName = 'Кіндрак Євген Миронович';
+let userAge = 35;
+let hobby = 'football';
+
+console.log(userName +'\n' , userAge +'\n' , hobby +'\n' );
+alert(userName +'\n' + userAge +'\n'  + hobby +'\n' )*/
+
+// завдання №9
+
+/*let  str1 = 'Хто ';
+let str2 = 'ти';
+let str3 = ' такий?';
+let concatenation = str1 +str2 +str3 ;
+document.write(concatenation);*/
+
+//завдання 11
+
+//console.log(parseInt('3.14' ), parseInt('-7.875'), parseInt("435"),  parseInt("Вася"));
+
+//завдання 12
+
+/*let str = prompt("Enter something", "ho-ho")
+console.log(str);*/
+
+//завдання 13
+
+//
+/*let a1 = 22;
+let b1 = 44;
+
+alert(a1 + b1)*/
+
+//завдання 14
+
+/*let nameUser = prompt('name');
+let surName = prompt('surname');
+let ageUser = prompt('age');
+
+alert('Доброго вечора' + ' '  + nameUser + ' ' + surName +  ' мои поздравления что вам ' + ageUser);*/
+
+
+//Додаткове
+
+//завдання 1
+
+/*let a = prompt('number A');
+let b = prompt('number B');
+let c = prompt('number C');
+
+if (a > b && a > c) {
+    if (b > c) {
+        console.log(c, b, a);
+    } else {
+        console.log(b, c, a);
+    }
+} else if (b > c && b > a) {
+    if (c > a) {
+        console.log(a, c, b)
+    } else {
+        console.log(c, a, b)
+    }
+}else if (c>b && c>a) {
+    if (b>a) {
+        console.log(a, b, c)
+    }else {
+        console.log(b, a, c)
+    }
+}
+
+*/
+
+//завдання 2
+
+/*let colorTrLight = prompt('Ввеси колір');
+
+if (colorTrLight === 'green') {
+    console.log('Йди')
+} else if (colorTrLight === 'yellow') {
+    console.log('Почекай')
+}else if (colorTrLight === 'red') {
+    console.log('Стій')
+}else if (colorTrLight === 'error') {
+    console.log('Роби що хочеш')
+}*/
+
+
+//завдання 3
+
+/*let colorTrLight = prompt('Ввеси колір');
+let isRoadClear = confirm('Наявність авто на дорозі')
+
+if (colorTrLight === 'green' && isRoadClear === true) {
+    console.log('Йди')
+} else if (colorTrLight === 'green' && isRoadClear === false) {
+    console.log('Почекати поки проїдуть порушники')
+}else if (colorTrLight === 'yellow' && isRoadClear === true) {
+    console.log('Почекай')
+}else if (colorTrLight === 'yellow' && isRoadClear === false) {
+    console.log('Почекай все одно')
+}else if (colorTrLight === 'red'  && isRoadClear === true) {
+    console.log('Стій все одно')
+}else if (colorTrLight === 'red'  && isRoadClear === false) {
+    console.log('Стій і чекай')
+}else  {
+    console.log('Роби що хочеш')
+}*/
